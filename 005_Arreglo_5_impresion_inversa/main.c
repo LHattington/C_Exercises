e14f5288004cfea40f1191bcdc6fe7986d473144
LHattington <148367518+LHattington@users.noreply.github.com>
2023-10-18 12:02:43 -0600

    Agregue un main

---
+++ 005_Arreglo_5_impresion_inversa/main.c
@@ -0,0 +1,16 @@
+/* Pide que ingreses cinco numeros, los vas a guardar en un arreglo y luego los vas a imprimir pero en orden inverso. */
+#include <stdio.h>
+
+int main()
+{
+    int array[5];
+    int num;
+    
+    for(num=0,num<=4,num++)
+    {
+        printf("Escribe 5 numeros: %d",i);
+        scanf("%d", &array[5]);
+    }
+   
+    return 0;
+}

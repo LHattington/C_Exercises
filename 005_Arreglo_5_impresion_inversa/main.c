dd0289781bb3d9c49c3dd680b3edfc35ab48f1aa
LHattington <148367518+LHattington@users.noreply.github.com>
2023-10-18 12:27:45 -0600

    Agregar la impresión del código de manera inversa.

--- 005_Arreglo_5_impresion_inversa/main.c
+++ 005_Arreglo_5_impresion_inversa/main.c
@@ -3,17 +3,22 @@
 
 int main()
 {
-    // En C, los arreglos se indexan a partir de la posicion cero (0).
+    // En C, los arreglos se indexan a partir de la posición cero (0).
     // Es decir, array[] tiene los espacios array[0], array[1], array[2], array[3] y array[4]
     int array[5];
-    
+
     printf("Escribe 5 numeros\n");
-    
+
     for(int pos=0; pos<=4; pos++)
     {
         printf("Escribe el numero %d: ", pos+1);
         scanf("%d", &array[pos]);
     }
-   
+
+    for(int pos=4; pos>=0; pos--)
+    {
+        printf("El numero %d es: %d\n", pos+1, array[pos]);
+    }
+
     return 0;
 }

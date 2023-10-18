18ac69a5dcd2bf516c865437bf25a663334dff53
LHattington <148367518+LHattington@users.noreply.github.com>
2023-10-18 12:29:57 -0600

    Agregue comentarios en el código

--- 005_Arreglo_5_impresion_inversa/main.c
+++ 005_Arreglo_5_impresion_inversa/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    // En C, los arreglos se indexan a partir de la posición cero (0).
+    // En C, los arreglos se indexan a partir de la posicion cero (0).
     // Es decir, array[] tiene los espacios array[0], array[1], array[2], array[3] y array[4]
     int array[5];
 
@@ -11,11 +11,11 @@ int main()
 
     for(int pos=0; pos<=4; pos++)
     {
-        printf("Escribe el numero %d: ", pos+1);
+        printf("Escribe el numero %d: ", pos+1); // Guarda los numeros en una posicion nueva del arreglo (0,1,2,3,...,n)
         scanf("%d", &array[pos]);
     }
 
-    for(int pos=4; pos>=0; pos--)
+    for(int pos=4; pos>=0; pos--)  // Hace la impresion del arreglo de manera inversa
     {
         printf("El numero %d es: %d\n", pos+1, array[pos]);
     }

90361cf1a1e62293ee68bc1124c4781ab98a6b49
LHattington <148367518+LHattington@users.noreply.github.com>
2023-10-18 10:04:55 -0600

    Cree un main para Par Impar

---
+++ 002_Par_Impar/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 0;
+    printf("Ingresa un número:  ");
+    scanf("%i",&numero);
+    
+    if (numero % 2 == 0){
+        printf("El número %i es par", numero);
+    }
+    else {
+        printf("El número %i es impar", numero);
+    }
+
+    return 0;
+}

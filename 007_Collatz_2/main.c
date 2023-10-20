df6e76b08f446d5e7ecb23e3bdd709ac238a5509
LHattington <148367518+LHattington@users.noreply.github.com>
2023-10-20 08:12:34 -0600

    Agregue comentarios sobre los cambios que faltan.

--- 007_Collatz_2/main.c
+++ 007_Collatz_2/main.c
@@ -6,9 +6,9 @@ int main()
     int num = 0; // Se ingresa un numero
     printf("Ingresa un numero:  ");
     scanf("%i",&num);
-    
-    if (num < 1)
-    {
+                                // Averiguar como hacer para que no permita numeros negativos ni numeros decimales 
+    if (num < 1)                // https://parzibyte.me/blog/2020/08/08/c-validacion-numeros/
+    {                           // https://es.stackoverflow.com/questions/76866/c%c3%b3mo-validar-el-tipo-de-entrada-en-c#:~:text=El%20c%C3%B3digo%20verifica%20que%20el%20n%C3%BAmero%20de%20par%C3%A1metros,%21%3D%201%29%20printf%20%28%22failuren%22%29%3B%20else%20printf%20%28%22valid%20integer%22%29%3B
         printf("No se puede hacer la conjetura de Collatz con %i", num);
     }
     

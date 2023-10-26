3fe1ed58be942f65ebd692274b4c7f9fa0b1f522
LHattington <148367518+LHattington@users.noreply.github.com>
2023-10-26 12:52:33 -0600

    Agregue un main

---
+++ 010_Estructura/main.c
@@ -0,0 +1,17 @@
+/* Asks names, last names, age, year of studies, grades for four subjects
+and a student unique numeric code for n students (up to 10) in a group. 
+Then ask for a student number and print its saved values as well as an average of the four grades. */
+#include <stdio.h>
+#include <string.h>
+
+struct Students {
+    char name[30];
+    char lastName[50]
+
+}
+
+int main()
+{
+
+return 0; 
+}

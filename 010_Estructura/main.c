4c5ed9b0b8437b997231d6c0e1a9ed92d6f4bda9
LHattington <148367518+LHattington@users.noreply.github.com>
2023-11-09 08:17:10 -0600

    Agregue una estructura

--- 010_Estructura/main.c
+++ 010_Estructura/main.c
@@ -3,12 +3,17 @@ and a student unique numeric code for n students (up to 10) in a group.
 Then ask for a student number and print its saved values as well as an average of the four grades. */
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-struct Students {
+struct Student {
     char name[30];
-    char lastName[50]
-
-}
+    char lastName[50];
+    unsigned int age;
+    unsigned int yearsOfStudy;
+    float grades[4];
+    unsigned int numericCode;
+}students[10];
 
 int main()
 {

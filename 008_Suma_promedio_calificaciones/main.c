684e82ae8c4a1d233b53421b1253ee869cc06aec
LHattington <148367518+LHattington@users.noreply.github.com>
2023-11-13 08:52:07 -0600

    Agregue comentarios al código.

--- 008_Suma_promedio_calificaciones/main.c
+++ 008_Suma_promedio_calificaciones/main.c
@@ -5,21 +5,21 @@
 
 int main()
 {
-   uint16_t numSubjects = 0;
+   uint16_t numSubjects = 0;                                                             // Declaracion de las variables
    float grade = 0;
    float sumGrades = 0;
    float average = 0;
 
-   printf("Enter the number of Subjects: ");
-   scanf("%hu",&numSubjects);
+   printf("Enter the number of Subjects: ");                                    
+   scanf("%hu",&numSubjects);                                                         // Imprime el numero de materias que se tienen
    printf("Enter the grades: ");
-   for (uint16_t counter = 1; counter <= numSubjects; counter++)
+   for (uint16_t counter = 1; counter <= numSubjects; counter++)     // Inicializacion del contador
    {
-        scanf("%f, ",&grade);
-        printf("%f + %f\n", sumGrades, grade);
-        sumGrades = grade + sumGrades;
+        scanf("%f, ",&grade);                                                               // Escribe las calificicaciones de las materias correspondientes
+        printf("%f + %f\n", sumGrades, grade);                                  // Imprime la suma de las calificaciones
+        sumGrades = grade + sumGrades;                                            // Suma las calificaciones y el valor se va sobrescribiendo en la variable "sumGrades"
    }
-   average = sumGrades / numSubjects;
+   average = sumGrades / numSubjects;                                          // Saca el promedio de todas las calificaciones ingresadas
    printf("The average is: %f", average);
    
    return 0;

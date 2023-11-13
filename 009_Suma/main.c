e942e5aa6e89b7672487d8a0a068716c427b5a75
LHattington <148367518+LHattington@users.noreply.github.com>
2023-11-13 09:11:47 -0600

    Agregue comentarios.

--- 009_Suma/main.c
+++ 009_Suma/main.c
@@ -3,10 +3,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
- 
-
 int main()
-{
+{                                                                                                                                                       // Se inicializan las variables.
    uint16_t numSubjects = 0;
    uint16_t numStudents = 0;
    float grade = 0;
@@ -14,35 +12,30 @@ int main()
    float totalAverage = 0;
    float sumAverage = 0;
 
- 
-
    printf("This program computes the average for n students with m subjects.\n");
    printf("Enter the number of Students: ");
-   scanf("%hu",&numStudents);
+   scanf("%hu",&numStudents);                                                                                                       // Imprime el numero de estudiantes que el usuario eligio
    printf("Enter the number of Subjects for each student: ");
-   scanf("%hu",&numSubjects);
+   scanf("%hu",&numSubjects);                                                                                                       // Imprime el numero de materias deseado
 
- 
-
-   for(uint16_t counterStudents = 1; counterStudents <= numStudents; counterStudents++)
+   for(uint16_t counterStudents = 1; counterStudents <= numStudents; counterStudents++)         // Se inicializa el contador
    {
-       float sumGrades = 0;
-
- 
+       float sumGrades = 0;                                                                                                                // Se inicializan las variables
 
        printf("Enter the grades for the student %i \n",counterStudents);
-       for (uint16_t counterSubjects = 1; counterSubjects <= numSubjects; counterSubjects++)
-       {
+       for (uint16_t counterSubjects = 1; counterSubjects <= numSubjects; counterSubjects++)     // Se inicializa el contador para ingresar las calificaciones
+       {                                                                                                                                              // dependiendo del numero de materias
            printf("Subject %i: ", counterSubjects);
-           scanf("%f, ",&grade);
-           printf("Sum of grades is = %f + %f\n", sumGrades, grade);
-           sumGrades = sumGrades + grade;
-        }
-       average = sumGrades / numSubjects;
-       printf("The student %hu average is: %f \n", counterStudents, average);
-       sumAverage = average + sumAverage;
-   }
-   totalAverage = sumAverage / numStudents;
-   printf("The average for the group is: %f", totalAverage);
+           scanf("%f, ",&grade);                                                                                                           // Imprime las calificaciones
+           printf("Sum of grades is = %f + %f\n", sumGrades, grade);                                                 // Imprime la suma de las calificaciones
+           sumGrades = sumGrades + grade;                                                                                         // Calcula la suma de las calificaciones y sobreescribe
+        }                                                                                                                                              // sumGrades para que guarde el valor mas reciente
+       average = sumGrades / numSubjects;                                                                                      // Calcula el promedio de las calificaciones ingresadas
+       printf("The student %hu average is: %f \n", counterStudents, average);                               // Imprime el numero de estudiante y el pomedio de este
+       sumAverage = average + sumAverage;                                                                                     // Calcula la suma de los promedios de los estudiantes y 
+   }                                                                                                                                                  // sobreescribe sumAverage patra guardar el valor mas reciente
+   totalAverage = sumAverage / numStudents;                                                                               // Calcula el promedio de los promedios de todos los estudiantes
+   printf("The average for the group is: %f", totalAverage);                                                          // Imprime el promedio tatal de un grupo de estudiantes
+  
    return 0;
 }

be492fa3be93105a13813e2ae13c3ee35cc18494
LHattington <148367518+LHattington@users.noreply.github.com>
2023-11-15 12:52:34 -0600

    Add comments.

--- 013_Tipos_de_datos_personalizados/main.c
+++ 013_Tipos_de_datos_personalizados/main.c
@@ -5,12 +5,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int number = 7;
+int number = 5;                                                                                                               // Define variables
 char group = 'A';
 float code = 1.0767;
 int numA = 0;
 int numB = 0;
-int * pointer_to_number = &number;
+int * pointer_to_number = &number;                                                                              // Define pointers
 char * pointer_to_group = &group;
 float * pointer_to_code = &code;
 int * pointer_to_numA = &numA;
@@ -18,26 +18,26 @@ int * pointer_to_numB = &numB;
  
 int main(){
  
-    printf("The value of \"number\" is: %d\n", number);
-    printf("The size of \"number\" is: %lu\n", sizeof(number));
-    printf("The memory address for \"number\" is: 0x%p\n", pointer_to_number);
-    printf("The \"group\" is: %c\n", group);
-    printf("The size of \"group\" is: %lu\n", sizeof(group));
-    printf("The memory address for \"group\" is: 0x%p\n", pointer_to_group);
-    printf("The numeric \"code\" value is: %f\n", code);
-    printf("The size of \"code\" is: %lu\n", sizeof(code));
-    printf("The memory address for \"code\" is: 0x%p\n", pointer_to_code);
+    printf("The value of \"number\" is: %d\n", number);                                                 // Print the value of the variable "number"
+    printf("The size of \"number\" is: %lu\n", sizeof(number));                                      // Print the size of the variable "number"
+    printf("The memory address for \"number\" is: 0x%p\n", pointer_to_number);       // Print the memory address of the variable "number"
+    printf("The \"group\" is: %c\n", group);                                                                    // Print the value of the variable "group"
+    printf("The size of \"group\" is: %lu\n", sizeof(group));                                           // Print the size of the variable "group"
+    printf("The memory address for \"group\" is: 0x%p\n", pointer_to_group);            // Print the memory address of the variable "group"
+    printf("The numeric \"code\" value is: %f\n", code);                                                // Print the value of the variable "code"
+    printf("The size of \"code\" is: %lu\n", sizeof(code));                                             // Print the size of the variable "code"
+    printf("The memory address for \"code\" is: 0x%p\n", pointer_to_code);              // Print the memory address of the variable "code"
  
     /* *pointer_to_number = 10;
     printf("The value of \"number\" is: %d\n", number);
     printf("The memory address for \"number\" is: 0x%p\n", pointer_to_number); */
     
-    printf("Enter a number: ");
-    scanf("%i", &numA);
+    printf("Enter a number: ");                                                                                      
+    scanf("%i", &numA);                                                                                                     
     printf("Enter another number: ");
     scanf("%i", &numB);
-    *pointer_to_numA = *pointer_to_numA + *pointer_to_numB;
-    printf("The value is: %i\n", *pointer_to_numA);
+    *pointer_to_numA = *pointer_to_numA + *pointer_to_numB;                                   // Save the value of the sum of "numA" and "numB" in the memory address of "numA"
+    printf("The value is: %i\n", *pointer_to_numA);                                                        // Print the latest value found in the memory address of "numA"
 
     return 0;
 }

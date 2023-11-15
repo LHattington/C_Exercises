ddfea4182328288346b156ce34ac10da7c3e5500
LHattington <148367518+LHattington@users.noreply.github.com>
2023-11-15 12:43:05 -0600

    Agregue una suma realizada con dos punteros.

--- 013_Tipos_de_datos_personalizados/main.c
+++ 013_Tipos_de_datos_personalizados/main.c
@@ -5,12 +5,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int number = 0;
+int number = 7;
 char group = 'A';
 float code = 1.0767;
+int numA = 0;
+int numB = 0;
 int * pointer_to_number = &number;
 char * pointer_to_group = &group;
 float * pointer_to_code = &code;
+int * pointer_to_numA = &numA;
+int * pointer_to_numB = &numB;
  
 int main(){
  
@@ -24,9 +28,16 @@ int main(){
     printf("The size of \"code\" is: %lu\n", sizeof(code));
     printf("The memory address for \"code\" is: 0x%p\n", pointer_to_code);
  
-    *pointer_to_number = 10;
+    /* *pointer_to_number = 10;
     printf("The value of \"number\" is: %d\n", number);
-    printf("The memory address for \"number\" is: 0x%p\n", pointer_to_number);
- 
+    printf("The memory address for \"number\" is: 0x%p\n", pointer_to_number); */
+    
+    printf("Enter a number: ");
+    scanf("%i", &numA);
+    printf("Enter another number: ");
+    scanf("%i", &numB);
+    *pointer_to_numA = *pointer_to_numA + *pointer_to_numB;
+    printf("The value is: %i\n", *pointer_to_numA);
+
     return 0;
 }

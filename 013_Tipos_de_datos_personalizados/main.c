e4a0a6c48fe7953b5c880a170b4ed14cc85afe7a
LHattington <148367518+LHattington@users.noreply.github.com>
2023-11-15 11:35:59 -0600

    Add the value, the size of and the memory address of the variables.

--- 013_Tipos_de_datos_personalizados/main.c
+++ 013_Tipos_de_datos_personalizados/main.c
@@ -1,7 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+// Print the size of an int pointer and the size of a char pointer and the size of a pointer to float or double
+// Ask the user to enter two numbers and add them using only pointers
 
-int main()
-{
-    
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int number = 0;
+char group = 'A';
+float code = 1.0767;
+int * pointer_to_number = &number;
+char * pointer_to_group = &group;
+float * pointer_to_code = &code;
+ 
+int main(){
+ 
+    printf("The value of \"number\" is: %d\n", number);
+    printf("The size of \"number\" is: %lu\n", sizeof(number));
+    printf("The memory address for \"number\" is: 0x%p\n", pointer_to_number);
+    printf("The \"group\" is: %c\n", group);
+    printf("The size of \"group\" is: %lu\n", sizeof(group));
+    printf("The memory address for \"group\" is: 0x%p\n", pointer_to_group);
+    printf("The numeric \"code\" value is: %f\n", code);
+    printf("The size of \"code\" is: %lu\n", sizeof(code));
+    printf("The memory address for \"code\" is: 0x%p\n", pointer_to_code);
+ 
+    *pointer_to_number = 10;
+    printf("The value of \"number\" is: %d\n", number);
+    printf("The memory address for \"number\" is: 0x%p\n", pointer_to_number);
+ 
+    return 0;
 }
